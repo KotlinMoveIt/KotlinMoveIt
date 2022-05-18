ff9da7baf0b7179a13d7ede49d3d85bdc4243bbf
Augusto Noronha <augustonoronha03@gmail.com>
2022-05-18 12:30:43 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/kotlinmoveit/Login/ValidaCodigoActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinmoveit.Login
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinmoveit.R
+
+class ValidaCodigoActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_valida_codigo)
+
+    }
+}
